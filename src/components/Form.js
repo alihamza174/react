b0d305b0764f31f71a7f80f47b8e19629d0f0f06
ali hamza <alitohamza123@gmail.com>
2025-01-04 01:16:58 +0500

    fix: form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,21 @@
-import Input  from "./Input"
-import {userFormContext} from "react-hook-form"
-import { AnimatePresernce, motion } from "framer-motion"
+import {useForm } from "react-hook-form"
 
 export const Form = () => {
-  const methods = useForm()
-  const onSubmit = methods.handleSubmit(data => {
-    console.log(data)
-  })
+  const {register, handleSubmit, formState: {errors}} = useForm({
+                                            defaultValues: {firstName: "ali", lastName: "hamza"}
+                                          })
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data))
+  }
   return (
     <>
-      <FormProvider {...methods}>
-        <Input label="name" type="text" id="name" placeholder="type your name..." />
-        <Input label="password" type="password" id="password" placeholder="type your password..." />
-      </FormProvider>
+      <form onSubmit={handleSubmit(onSubmit)} className="formControl">
+      <input {...register("firstName", {required: true})} className="form-control mt-1" />
+      {errors.firstName?.type == "required" && (<p className="errors">First name is required</p>)}
+      <input {...register("lastName", {required: true})} className="form-control mt-1" />
+      {errors.lastName?.type == "required" && (<p className="errors">Last name is required</p>)}
+      <input type="submit" className="btn btn-primary" />
+      </form>
     </>
   )
 }
